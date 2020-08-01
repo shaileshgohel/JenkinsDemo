2962cb8883d3c42acc49e67075f873293c089b9d
Ubuntu <ubuntu@ip-172-31-29-2.ap-south-1.compute.internal>
2020-08-01 16:43:20 +0000

    change the firefox driver

--- src/test/java/com/maven/test/MaventTest.java
+++ src/test/java/com/maven/test/MaventTest.java
@@ -1,9 +1,12 @@
 package com.maven.test;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
@@ -13,9 +16,14 @@ public class MaventTest {
 	@Test
 	public void firstTest() throws MalformedURLException {
 		System.out.println("This is our first Test");
-		System.setProperty("webdriver.gecko.driver", "/home/ubuntu/libs/geckodriver");
-		driver = new FirefoxDriver();
+		/*System.setProperty("webdriver.gecko.driver", "/home/ubuntu/libs/geckodriver");
+		driver = new FirefoxDriver();*/
 		
+		DesiredCapabilities cap = DesiredCapabilities.firefox();
+		cap.setCapability("platFormName", "windows");
+		
+		URL url = new URL ("http://172.17.0.1:4444/wd/hub");
+		WebDriver driver = new RemoteWebDriver(url,cap);
 		
 		driver.get("https://www.simplilearn.com/");
 	}
